3023c89ed1943d1a780dd9cadd607959b102d59e
Jerome <Jerome@Jerome-PC>
2012-04-15 21:20:07 +0200

    Modification pourle  reset des noeuds pour permettre de relancer l'application plusieurs fois.

--- src/carpgenetique/comportement/ate/AfficherSolution.java
+++ src/carpgenetique/comportement/ate/AfficherSolution.java
@@ -1,9 +1,16 @@
 package carpgenetique.comportement.ate;
 
+import carp.SolutionCARP;
 import carpgenetique.agent.ATEPop;
 import carpgenetique.algo.Individu;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.TickerBehaviour;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import seisco.probleme.Solution;
 
 /**
@@ -28,8 +35,24 @@ public class AfficherSolution extends Behaviour {
         if(ate.getNbAgentsArretes() >= ate.getNbAgents()) {
             Solution ind = ate.getCacheSolution();
             if(ind != null) {
-                ind.afficher();
-                this.done = true;
+                FileOutputStream fos = null;
+                PrintStream out = null;
+                try {
+                    fos = new FileOutputStream("resultats.txt");
+                    out = new PrintStream(fos);
+                    ate.println(((SolutionCARP)ind).getResultat().toString(), out);
+                    
+                    this.done = true;
+                } catch(FileNotFoundException ex) {
+                    Logger.getLogger(AfficherSolution.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    try {
+                        fos.close();
+                        out.close();
+                    } catch(IOException ex) {
+                        Logger.getLogger(AfficherSolution.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             } else {
                 ate.println("Aucune solution à afficher");
             }
